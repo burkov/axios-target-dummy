fbb13a9696b1e9cedb6c83d4ede91a8cea87335b
alexander.burkov <alexander.burkov@jetbrains.com>
2020-03-22 20:23:43 +0300

    Better start script, nodemon config

--- bin/www.ts
+++ bin/www.ts
@@ -1,3 +1,37 @@
 import app from '../src/index';
+import http from 'http';
 
-app.listen(3000);
+const server = http.createServer(app);
+const port = 3000;
+
+const onError = (error: NodeJS.ErrnoException) => {
+  const { code, syscall } = error;
+  if (syscall !== 'listen') throw error;
+  switch (code) {
+    case 'EACCESS':
+      console.log('Access denied.');
+      break;
+    case 'EADDRINUSE':
+      console.log('Address in use.');
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  console.log('Listening on port', port);
+};
+
+const onConnection = (socket: NodeJS.Socket) => {};
+
+const onClose = () => {
+  console.log('Client disconnected');
+};
+
+server.on('error', onError);
+server.on('listening', onListening);
+server.on('close', onClose);
+server.on('connection', onConnection);
+
+server.listen(port);
--- src/responseProbe/index.ts
+++ src/responseProbe/index.ts
@@ -5,11 +5,19 @@ const router = express.Router();
 
 router.post<RouteParams>('/:status(\\d{3})/:shape(valid|malformed)', (req, res) => {
   const { status, shape } = req.params;
-  const parsed = parseInt(status, 10);
-  res.status(parsed);
-  const isValid = shape === 'valid';
+  const parsedStatus = parseInt(status, 10);
+  const isSucess = parsedStatus >= 200 && parsedStatus <= 400;
+  res.status(parsedStatus);
+  if (shape === 'malformed') {
+    res.send('malformed body');
+    return;
+  }
 
-  res.send(req.params);
+  const response = isSucess
+    ? { result: 'some result' }
+    : { errors: [{ type: 'Type', title: 'Error title', detail: 'Error detail' }] };
+
+  res.send(response);
 });
 
 export default router;
